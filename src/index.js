fcba653e5850fcfaebcad98c63b69e282941d8df
ANKITKUMARBARIK <barikankitkumar2@gmail.com>
2025-06-01 16:03:09 +0530

    add admin seeding functionality and role configuration

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import app from "./app.js";
+import seedAdmin from "./seed/seedAdmin.js";
 
 dotenv.config({ path: "./.env" });
 
 connectDB
-    .then(() => {
+    .then(async () => {
+        await seedAdmin();
         const server = app.listen(process.env.PORT || 8000, () =>
             console.log(`Server started at PORT: ${process.env.PORT}`)
         );
---
+++ src/seed/seedAdmin.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+import ROLES from "../config/role.js";
+
+const seedAdmin = async () => {
+    try {
+        const existedAdmin = await User.findOne({ role: ROLES.ADMIN });
+        if (!existedAdmin) {
+            const user = new User({
+                fullName: "Admin",
+                username: "admin",
+                email: process.env.ADMIN_EMAIL,
+                password: process.env.ADMIN_PASSWORD,
+                avatar: "avatar.png",
+                coverImage: "coverImage.png",
+                timezone: "Asia/Kolkata",
+                role: ROLES.ADMIN,
+                isVerified: true,
+            });
+            await user.save();
+            console.info("default admin created successfully");
+        } else {
+            console.info("admin already exists");
+        }
+    } catch (error) {
+        console.error("error while creating default admin:", error);
+    }
+};
+
+export default seedAdmin;
